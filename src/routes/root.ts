fd8cac4c219a250ee1b9b02d0ea72e72c811e3ea
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-09-22 01:33:59 +0800

    feat: get data from dynamodb

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,31 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { dynamoConfig } from "../utils";
+import { Product } from "../models/productInterfaces";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+const client = new DynamoDBClient(dynamoConfig());
+const dynamodb = DynamoDBDocumentClient.from(client);
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/products", async function (request, reply) {
+    try {
+      const command = new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+      });
+
+      const response = await dynamodb.send(command);
+      const products: Product[] = response.Items?.map((item) =>
+        unmarshall(item)
+      ) as Product[];
+
+      return { products };
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      reply.status(500).send({ error: "Failed to fetch products" });
+    }
+  });
+};
 
 export default root;
